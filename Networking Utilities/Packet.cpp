1dd560449112a65c4bbba5658364c00631b7bc35
edwardrowens <eddiebeme@gmail.com>
2016-06-25 18:40:56 -0700

    packet class in and utilities dependency added

--- Networking Utilities/Packet.cpp
+++ Networking Utilities/Packet.cpp
@@ -1,12 +1,1 @@
 #include "Packet.h"
-
-template<typename T>
-asio::mutable_buffer Packet<T>::toAsioBuffer() {
-	return asio::buffer((char*)&data->front(), sizeof(T) * data.size());
-}
-
-
-template<typename T>
-void Packet<T>::add(T element) {
-	data.push_back(element);
-}
--- Networking Utilities/Packet.h
+++ Networking Utilities/Packet.h
@@ -6,8 +6,13 @@
 template<typename T>
 class Packet {
 public:
-	asio::mutable_buffer toAsioBuffer();
-	void add(T element);
+	asio::mutable_buffers_1 toAsioBuffer() {
+		return asio::buffer((char*)&data.front(), sizeof(T) * data.size());
+	}
+
+	void add(T element) {
+		data.push_back(element);
+	}
 private:
 	std::vector<T> data;
 };
